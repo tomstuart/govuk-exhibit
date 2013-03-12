2287169d43f5eb1d8939f91b04f42bac2ae40c09
Tom Stuart <tom@codon.com>
2013-03-12 20:27:14 +0000

    Synchronise current URLs on link click

--- mirror.js
+++ mirror.js
@@ -14,15 +14,40 @@
     }
   };
 
+  var navigateTo = function (url) {
+    if (window.location.href !== url) {
+      window.location.href = url;
+    }
+  };
+
   var beginControlling = function (faye) {
     window.addEventListener('scroll', function () {
       faye.publish('/scroll', { x: window.scrollX, y: window.scrollY });
+    });
+
+    window.addEventListener('click', function (event) {
+      var element = event.target;
+
+      while (element) {
+        if (element.localName === 'a') {
+          event.preventDefault();
+          faye.publish('/navigate', { url: element.href });
+          navigateTo(element.href);
+          break;
+        }
+
+        element = element.parentNode;
+      }
     });
   };
 
   var beginMirroring = function (faye) {
     faye.subscribe('/scroll', function (message) {
       window.scrollTo(message.x, message.y);
+    });
+
+    faye.subscribe('/navigate', function (message) {
+      navigateTo(message.url);
     });
   };
 
